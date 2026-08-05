30f911399ea6511cadb070960694fc4b5cad324f
Monami <monami.saeki@gmail.com>
2026-08-05 13:04:16 +0300

    added another playwright test

--- e2e-tests/pokedex.spec.js
+++ e2e-tests/pokedex.spec.js
@@ -10,4 +10,11 @@ describe('Pokedex', () => {
       )
     ).toBeVisible()
   })
+  test('one can navigate from the main page to the page of a particular Pokemon', async ({
+    page
+  }) => {
+    await page.goto('')
+    await page.getByText('ivysaur').click()
+    await expect(page.getByText('chlorophyll')).toBeVisible()
+  })
 })
